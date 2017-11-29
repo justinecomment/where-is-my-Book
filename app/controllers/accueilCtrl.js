b325ab8dbafb8bbb64535330fc0d98e2ab1376d3
Justine <justinecomment@gmail.com>
2017-11-29 15:55:47 +0100

    + change backend DELETE Method.

--- app/controllers/accueilCtrl.js
+++ app/controllers/accueilCtrl.js
@@ -1,31 +1,31 @@
 myApp.controller('accueilCtrl', function($scope, booksService, LxNotificationService, $location) {
 
-    booksService.getBookPrete().then(function(result){
-        $scope.booksLists = result.data;
-    });
+    // booksService.getBookPrete().then(function(result){
+    //     $scope.booksLists = result.data;
+    // });
 
-    $scope.rendu = function(){
-       var name_pret = this.booksList.nom;
+    // $scope.rendu = function(){
+    //    var name_pret = this.booksList.nom;
        
-        LxNotificationService.confirm('', 'Confirmer?',
-                {
-                   cancel: 'Rendu',
-                   ok    : 'Cancel'
-                }, function(editer){
-                    if (editer){
-                        $location.path('/');
-                    }
-                    else{
-                        booksService.rendu(name_pret).then(function(result){
-                            booksService.getBookPrete().then(function(result){
-                                 $scope.booksLists = result.data;
-                            })
-                            LxNotificationService.error('Le livre a bien été rendu');
-                        })
+    //     LxNotificationService.confirm('', 'Confirmer?',
+    //             {
+    //                cancel: 'Rendu',
+    //                ok    : 'Cancel'
+    //             }, function(editer){
+    //                 if (editer){
+    //                     $location.path('/');
+    //                 }
+    //                 else{
+    //                     booksService.rendu(name_pret).then(function(result){
+    //                         booksService.getBookPrete().then(function(result){
+    //                              $scope.booksLists = result.data;
+    //                         })
+    //                         LxNotificationService.error('Le livre a bien été rendu');
+    //                     })
                        
-                    }
-        });
+    //                 }
+    //     });
 
-    }
+    // }
    
 });
--- app/services/booksServices.js
+++ app/services/booksServices.js
@@ -1,5 +1,5 @@
 myApp.service('booksService', function($http){
-    const baseUrl = 'http://localhost/whereIsMyBook';
+    const baseUrl = "http://localhost/whereIsMyBook-symfony/web/app_dev.php";
     var bookSaved;
 
     this.saveBook= function(book){
@@ -10,89 +10,94 @@ myApp.service('booksService', function($http){
         return bookSaved;
     };
 
-    this.postBook = function(dataToPost){
-        return $http({ 
-            method: 'POST', 
-            url: baseUrl + '/books.php?add_book=', 
-            dataType: 'json', 
-            data: dataToPost,
-            headers: {
-            'Content-Type': 'application/json'
-            }
-        });
-    };
+    // this.postBook = function(dataToPost){
+    //     return $http({ 
+    //         method: 'POST', 
+    //         url: baseUrl + '/addbook', 
+    //         dataType: 'json', 
+    //         data: dataToPost,
+    //         headers: {
+    //         'Content-Type': 'application/json'
+    //         }
+    //     });
+    // };
 
     this.getBooks = function(){
-         return $http.get(baseUrl + '/books.php').success(function(result){
+         return $http.get(baseUrl + '/listbook').success(function(result){
             return result.data;
         })
     };
 
-    this.getBookPrete = function(){
-         return $http.get(baseUrl + '/books.php?book_prete=').success(function(result){
-            return result.data;
-        })
-    }
+    // this.getBookPrete = function(){
+    //      return $http.get(baseUrl + '/books.php?book_prete=').success(function(result){
+    //         return result.data;
+    //     })
+    // }
 
-    this.rendu = function(nom_pret){
-        return $http({
-            method  : 'DELETE',
-            url: baseUrl + '/books.php?rendu=' + nom_pret,
-            data    : {"nom_pret":  nom_pret },
-            headers : {'Content-Type': 'application/x-www-form-urlencoded'}
-        })
-    }
+    // this.rendu = function(nom_pret){
+    //     return $http({
+    //         method  : 'DELETE',
+    //         url: baseUrl + '/books.php?rendu=' + nom_pret,
+    //         data    : {"nom_pret":  nom_pret },
+    //         headers : {'Content-Type': 'application/x-www-form-urlencoded'}
+    //     })
+    // }
 
      this.deleteBook = function(index){
         return $http({
             method  : 'DELETE',
-            url: baseUrl + '/books.php?deleteBook=' + index,
-            data    : {"id_book":  index },
-            headers : {'Content-Type': 'application/x-www-form-urlencoded'}
-        })
-    };
-
-    this.editBook= function(formData){
-         return $http({
-            method : 'PUT',
-            url : baseUrl + '/books.php?edit_book=',
-            dataType: 'json',
-            data : formData,
+            url: baseUrl + '/deleteBook/' + index,
+            data    : {"idBook":  index },
             headers: {
-                'Content-Type': 'application/json'
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin' : '*'
             }
         })
+
     };
 
-    this.addPret = function(dataToPost){
-        return $http({ 
-            method: 'POST', 
-            url: baseUrl + '/books.php?add_pret=', 
-            dataType: 'json', 
-            data: dataToPost,
-            headers: {
-            'Content-Type': 'application/json'
-            }
-        });
-    };
+    // this.editBook= function(formData){
+    //      return $http({
+    //         method : 'PUT',
+    //         url : baseUrl + '/books.php?edit_book=',
+    //         dataType: 'json',
+    //         data : formData,
+    //         headers: {
+    //             'Content-Type': 'application/json'
+    //         }
+    //     })
+    // };
 
-    this.modifyPret = function(nom, idBook){
-         return $http({
-            method : 'PUT',
-            url : baseUrl + '/books.php?modifyPret=',
-            dataType: 'json',
-            data : {'nom': nom , 'idBook': idBook},
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        })
-    };
+    // this.addPret = function(dataToPost){
+    //     return $http({ 
+    //         method: 'POST', 
+    //         url: baseUrl + '/books.php?add_pret=', 
+    //         dataType: 'json', 
+    //         data: dataToPost,
+    //         headers: {
+    //         'Content-Type': 'application/json'
+    //         }
+    //     });
+    // };
 
-    this.searchBook = function(searchValue){
-        return $http.get(baseUrl + '/books.php?search=' + searchValue).success(function(result){
-            return result.data;
-        })
-    }
+    // this.modifyPret = function(nom, idBook){
+    //      return $http({
+    //         method : 'PUT',
+    //         url : baseUrl + '/books.php?modifyPret=',
+    //         dataType: 'json',
+    //         data : {'nom': nom , 'idBook': idBook},
+    //         headers: {
+    //             'Content-Type': 'application/json'
+    //         }
+    //     })
+    // };
+
+    // this.searchBook = function(searchValue){
+    //     return $http.get(baseUrl + '/books.php?search=' + searchValue).success(function(result){
+    //         return result.data;
+    //     })
+    // }
 
 
 });
